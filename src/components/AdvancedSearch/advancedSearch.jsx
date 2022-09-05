d9323dec40fabcd69b8ea0ae422269cb5d23bc30
Riyadh Alsalemi <riyadh@Riyadhs-MacBook-Pro.local>
2022-09-04 21:37:10 -0500

    update

--- src/components/AdvancedSearch/advancedSearch.jsx
+++ src/components/AdvancedSearch/advancedSearch.jsx
@@ -9,6 +9,7 @@ import "./advancedSearch.css";
 import Popper from "@mui/material/Popper";
 import { createStyles } from "@mui/material/styles";
 import { makeStyles } from "@mui/styles";
+import { IoIosArrowForward, IoIosArrowDown } from "react-icons/io";
 import AdvancedSearchData from "./AdvancedSearchData.js";
 
 const useStyles = makeStyles((theme) =>
@@ -16,22 +17,33 @@ const useStyles = makeStyles((theme) =>
     root: {
       "& .MuiAutocomplete-listbox": {
         fontSize: 12,
+        height: 150,
         zindex: 1,
-        "& li:nth-child(even)": { backgroundColor: "#CCC" },
-        "& li:nth-child(odd)": { backgroundColor: "#FFF" },
+        "& li": { backgroundColor: "#FFF" },
       },
     },
     textfield: {
+      "& .MuiInputBase-inputSizeSmall": {
+        fontSize: 10,
+      },
       "& .MuiInputBase-sizeSmall": {
         fontSize: 12,
-        height: 29,
+        height: 22,
       },
       "& .MuiInputLabel-sizeSmall": {
-        fontSize: 14,
+        fontSize: 10,
         marginTop: -5,
       },
       "& .MuiSvgIcon-fontSizeSmall": {
-        fontSize: 16,
+        fontSize: 12,
+        color: "blue",
+      },
+      "& .MuiAutocomplete-inputFocused": {
+        marginTop: -4,
+        fontSize: 10,
+      },
+      "& .MuiFormLabel-colorPrimary": {
+        marginTop: 4,
       },
     },
   })
@@ -46,62 +58,113 @@ function App() {
   const [search, setSearch] = useState("");
   const [search1, setSearch1] = useState("");
   const [search2, setSearch2] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen1, setIsOpen1] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
 
   const u = [...new Set(data.map((a) => a.email))];
 
   return (
     <div className="home">
       <form className="advanced_search_form">
-        <Box
-          sx={{
-            "& > :not(style)": { m: 1, width: "25ch", height: 20 },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            id="outlined-basic"
-            size="small"
-            label="Outlined"
-            variant="outlined"
-            className={classes.textfield}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-        </Box>
-        <Stack sx={{ width: 200 }}>
-          <Autocomplete
-            size="small"
-            options={u}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Pillar"
-                className={classes.textfield}
-              />
+        <div className="input_title">
+          <div>Keyword</div>
+          <div className="input_title_icon1">
+            {" "}
+            {isOpen ? (
+              <IoIosArrowDown onClick={() => setIsOpen(!isOpen)} />
+            ) : (
+              <IoIosArrowForward onClick={() => setIsOpen(!isOpen)} />
             )}
-            onChange={function (e, newValue) {
-              return setSearch1(newValue === null ? "" : newValue);
-            }}
-            PopperComponent={CustomPopper}
-          />
-        </Stack>
-        <Stack sx={{ width: 200, border: "none", outline: "none" }}>
-          <Autocomplete
-            options={data.map((a) => a.phone)}
-            size="small"
-            renderInput={(params) => (
+          </div>
+        </div>
+        {isOpen && (
+          <div className="search">
+            <Box
+              sx={{
+                "& > :not(style)": { width: 180, height: 20, fontSize: 10 },
+              }}
+              noValidate
+              autoComplete="off"
+            >
               <TextField
-                {...params}
-                label="phone"
+                id="outlined-basic"
+                size="small"
+                // label="Outlined"
+                placeholder="Keyword"
+                variant="outlined"
                 className={classes.textfield}
+                onChange={(e) => setSearch(e.target.value)}
               />
+            </Box>
+          </div>
+        )}
+        <div className="input_title1">
+          <div>Pillar</div>
+          <div style={{ marginLeft: 14 }} className="input_title_icon1">
+            {" "}
+            {isOpen1 ? (
+              <IoIosArrowDown onClick={() => setIsOpen1(!isOpen1)} />
+            ) : (
+              <IoIosArrowForward onClick={() => setIsOpen1(!isOpen1)} />
             )}
-            onChange={(e, newValue) => {
-              return setSearch2(newValue === null ? "" : newValue);
-            }}
-            PopperComponent={CustomPopper}
-          />
-        </Stack>
+          </div>
+        </div>
+        {isOpen1 && (
+          <div className="search1">
+            <Stack sx={{ width: 180 }}>
+              <Autocomplete
+                size="small"
+                options={u}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    // label="Pillar"
+                    placeholder="Pillar"
+                    className={classes.textfield}
+                  />
+                )}
+                onChange={function (e, newValue) {
+                  return setSearch1(newValue === null ? "" : newValue);
+                }}
+                PopperComponent={CustomPopper}
+              />
+            </Stack>
+          </div>
+        )}
+        <div className="input_title2">
+          <div>Product</div>
+          <div className="input_title_icon2">
+            {" "}
+            {isOpen2 ? (
+              <IoIosArrowDown onClick={() => setIsOpen2(!isOpen2)} />
+            ) : (
+              <IoIosArrowForward onClick={() => setIsOpen2(!isOpen2)} />
+            )}
+          </div>
+        </div>
+        {isOpen2 && (
+          <div className="search2">
+            <Stack sx={{ width: 180 }}>
+              <Autocomplete
+                options={data.map((a) => a.phone)}
+                size="small"
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    // label="phone"
+                    placeholder="Product"
+                    className={classes.textfield}
+                  />
+                )}
+                onChange={(e, newValue) => {
+                  return setSearch2(newValue === null ? "" : newValue);
+                }}
+                PopperComponent={CustomPopper}
+              />
+            </Stack>
+          </div>
+        )}
       </form>
       <AdvancedSearchData search={search} search1={search1} search2={search2} />
     </div>
